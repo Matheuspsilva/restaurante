400a253f75b790ae756639fdb3b335a80a018b63
Matheus Silva <matheuspsilva29@gmail.com>
2019-09-25 23:11:13 -0300

    segundo commit listar e deletar 2 25-09-19

---
+++ resources/views/admin/restaurants/index.blade.php
@@ -0,0 +1,24 @@
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Criado em</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach ($restaurants as $r)
+    <tr>
+    <td>{{$r->id}}</td>
+        <td>{{$r->name}}</td>
+        <td>{{$r->created_at}}</td>
+        <td>
+        <a href="{{route('restaurant.edit', ['restaurant' => $r -> id])}}">Editar</a>
+        <a href="{{route('restaurant.remove', ['id' => $r -> id])}}">Excluir</a>
+
+        </td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
