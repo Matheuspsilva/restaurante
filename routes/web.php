832f8005ee52d51f391687db9decf736e5530a95
Matheus Silva <matheuspsilva29@gmail.com>
2019-10-08 16:31:57 -0300

    correção de rotas

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 });
 
 Route::prefix('admin')->group(function(){
-    Route::get('restaurants','admin\\RestaurantController@index')->name('restaurant.index');//name = apelido da rota
-    Route::get('restaurants/new','admin\\RestaurantController@new')->name('restaurant.new');
-    Route::post('restaurants/store','admin\\RestaurantController@store')->name('restaurant.store');
+    Route::get('restaurants','Admin\\RestaurantController@index')->name('restaurant.index');//name = apelido da rota
+    Route::get('restaurants/new','Admin\\RestaurantController@new')->name('restaurant.new');
+    Route::post('restaurants/store','Admin\\RestaurantController@store')->name('restaurant.store');
     Route::get('restaurants/edit/{restaurant}', 'Admin\\RestaurantController@edit')->name('restaurant.edit');
     Route::post('restaurants/update/{id}', 'Admin\\RestaurantController@update')->name('restaurant.update');
     Route::get('restaurants/remove/{id}', 'Admin\\RestaurantController@delete')->name('restaurant.remove');
