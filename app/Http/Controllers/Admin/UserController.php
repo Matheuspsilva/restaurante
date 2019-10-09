01ac32a1ba3721768ce378458d1aeefbbe4424da
Matheus Silva <matheuspsilva29@gmail.com>
2019-10-09 16:46:13 -0300

    adicionando UserController e UserRequest

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function new(){
+        return view('admin.users.store');
+    }
+
+    public function store(UserRequest $request){
+        $userData = $request->all();
+        $request->validated();
+        $user = new User();
+        $user->create($userData);
+
+    print "Usuário criado com sucesso";
+    }
+
+    public function edit(User $user){
+        return view('admin.users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, $id){
+        $userData = $request->all();
+        $request->validated();
+        $user =  User::findOrFail($id);
+        $user->update($userData);
+
+        print "User atualizado com sucesso";
+    }
+
+    public function delete($id){
+
+        $user =  User::findOrFail($id);
+        $user->delete();
+
+        print "User removido com sucesso";
+    }
+
+
+}
