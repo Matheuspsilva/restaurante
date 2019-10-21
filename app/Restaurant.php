25b21946e09a330a98376ce853d771fc07cbb091
Matheus Silva <matheuspsilva29@gmail.com>
2019-10-21 11:04:54 -0300

    add restaurant_photos model and migration

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -19,4 +19,8 @@ class Restaurant extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function photos(){
+        return $this->hasMany(RestaurantPhoto::class);
+    }
+
 }
